8779f9fee9766b8b95b5e65e9d4631a9358c77af
HNikki0303 <nikitapant496@gmail.com>
2026-09-24 23:44:12 +0530

    user routes

---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { register , login , getProfile} from "../controllers/AuthControllers.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+
+const router = express.Router();
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/me", protect, getProfile);
+
+export default router;
