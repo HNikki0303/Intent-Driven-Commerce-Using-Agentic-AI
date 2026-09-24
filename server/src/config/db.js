e251c5cb0c7450680644dc223396ee5b74e71c7c
HNikki0303 <nikitapant496@gmail.com>
2026-09-24 23:20:38 +0530

    configure database connection

---
+++ server/src/config/db.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
+
+const connectDB = async()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected : ${conn.connection.host}`)
+
+    }catch(error){
+        console.error("Database connection failed !");
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
