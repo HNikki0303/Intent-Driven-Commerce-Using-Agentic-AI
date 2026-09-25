b51f03bd35febd139819300165a6871b9a37229e
HNikki0303 <nikitapant496@gmail.com>
2026-09-25 15:39:29 +0530

    auth middleware for authentication and authorisation

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,56 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({
+                success: false,
+                message: "Authorization header missing"
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.id).select("-password");
+        if (!user) {
+        return res.status(401).json({
+            success: false,
+            message: "User no longer exists"
+        });
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid or expired token"
+        });
+    }
+};
+
+export const authorize = (...roles) => {
+
+    return (req, res, next) => {
+
+        if (!roles.includes(req.user.role)) {
+
+            return res.status(403).json({
+                success: false,
+                message: "Access denied"
+            });
+
+        }
+
+        next();
+
+    };
+
+};
