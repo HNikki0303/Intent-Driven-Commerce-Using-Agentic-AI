581afbc8b23dd973ddb2dbaae2c6890f91db2e9a
HNikki0303 <nikitapant496@gmail.com>
2026-09-25 15:37:15 +0530

    cloudinary configuration

--- server/src/config/Cloudinary.js
+++ server/src/config/Cloudinary.js
@@ -3,16 +3,17 @@ import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
+//just for checking ;
 console.log({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret_loaded: !!process.env.CLOUDINARY_API_SECRET
+});
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
